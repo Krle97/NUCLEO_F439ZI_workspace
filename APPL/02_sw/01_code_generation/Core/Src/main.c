f000212ffe60d9ee109d71506a71419ad97cb590
Krle97 <igorkrstic.sm@gmail.com>
2023-02-26 21:57:51 +0100

    [Software] Disabling unnecessary features

--- APPL/02_sw/01_code_generation/Core/Src/main.c
+++ APPL/02_sw/01_code_generation/Core/Src/main.c
@@ -19,9 +19,6 @@
 /* Includes ------------------------------------------------------------------*/
 #include "main.h"
 #include "cmsis_os.h"
-#include "eth.h"
-#include "usart.h"
-#include "usb_otg.h"
 #include "gpio.h"
 
 /* Private includes ----------------------------------------------------------*/
@@ -89,9 +86,6 @@ int main(void)
 
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
-  MX_ETH_Init();
-  MX_USART3_UART_Init();
-  MX_USB_OTG_FS_PCD_Init();
   /* USER CODE BEGIN 2 */
 
   /* USER CODE END 2 */
